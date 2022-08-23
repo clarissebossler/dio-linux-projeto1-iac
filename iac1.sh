e087630cba77c61db7ba354ade7049884a37d11e
clarissebossler <clarisse.bossler@gmail.com>
2022-08-23 17:29:14 -0300

    com alterações após teste

--- iac1.sh
+++ iac1.sh
@@ -13,24 +13,31 @@ groupadd GRU_ADM
 groupadd GRU_VEN
 groupadd GRU_SEC
 
-echo "Criando usuários . . . . . . ."
+echo "Criando usuários e adicionando os usuários aos grupos . . . . . . ."
 
-useradd -c 'Maria Nieves' -G GRU_VEN chiquinha
-useradd -c 'Ramon Valdes' -G GRU_VEN madruga
-useradd -c 'Carlos Villagran' -G GRU_VEN quico
+useradd -m -s /bin/bash -c 'Maria Nieves' -G GRU_VEN chiquinha
+useradd -m -s /bin/bash -c 'Ramon Valdes' -G GRU_VEN madruga
+useradd -m -s /bin/bash -c 'Carlos Villagran' -G GRU_VEN quico
 
-useradd -c 'Angelina Fernandez' -G GRU_ADM clotilde
-useradd -c 'Roberto Bolanos' -G GRU_ADM chaves
-useradd -c 'Edgar Vivar' -G GRU_ADM nhonho
+useradd -m -s /bin/bash -c 'Angelina Fernandez' -G GRU_ADM clotilde
+useradd -m -s /bin/bash -c 'Roberto Bolanos' -G GRU_ADM chaves
+useradd -m -s /bin/bash -c 'Edgar Vivar' -G GRU_ADM nhonho
 
-useradd -c 'Florinda Meza' -G GRU_SEC florinda
-useradd -c 'Ruben Aguirre' -G GRU_SEC girafales
-useradd -c 'Raul Padilla' -G GRU_SEC jaiminho
+useradd -m -s /bin/bash -c 'Florinda Meza' -G GRU_SEC florinda
+useradd -m -s /bin/bash -c 'Ruben Aguirre' -G GRU_SEC girafales
+useradd -m -s /bin/bash -c 'Raul Padilla' -G GRU_SEC jaiminho
 
 echo "Especificando permissões dos diretórios . . . . . . ."
+
+chown root:GRU_ADM /adm
+chown root:GRU_VEN /ven
+chown root:GRU_SEC /sec
+
 chmod 777 /publico
 chmod 750 /adm
 chmod 750 /ven
 chmod 750 /sec
 
 echo "Concluído"
+
+echo "Para desfazer as alterações no sistema, executar o script xxxxx"
